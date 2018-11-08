d9b9c3c0c6c0b75239af68ed1c11dec0fb8a5776
Adam <adamh93@verizon.net>
2018-11-07 21:33:10 -0500

    done: Create Boilerplate files, Create SearchBar.js, edit SearchBar.js and make a component SearchBar to enter in weather location, create WeatherDisplay.js file to later user to show weather received from OpenWeatherAPI; task: Test SearchBar component in app, build out component WeatherAPI in weatherDisplay.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { SearchBar } from './SearchBar.js'
+
+
+
+class weatherApp extends React.Component {
+
+  <div className="app">
+    
+  </div>
+}
+
+ReactDOM.render(
+  <weatherApp />,
+   document.getElementById('app')
+ );
