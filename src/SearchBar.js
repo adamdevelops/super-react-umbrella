0a031a7927627e084e4c57e84fdf9d012583cbc3
Adam <adamh93@verizon.net>
2018-11-15 22:48:15 -0500

    done: done: Move API call from SearchBar.js to index.js, In index.js bind this.componentDidMount to this keyword, Modify in componentDidMount to accept search term value, Create a const city equal to term parameter for function, Modify conditonal statement to only make API call if const city has a value, Pass down componentDidMount func as a prop called onSearchTermChange into the SearchBar component in Weather App component render, Edited handleSubmit() within SearchBar.js to store search term value in the passed down prop

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-
-const API_KEY = '992e628de9fb7c2d45c8a82dc61259fc';
+import React, { Component } from 'react'
 
 
 export default class SearchBar extends Component {
@@ -12,8 +10,6 @@ export default class SearchBar extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.componentDidMount = this.componentDidMount.bind(this);
-
     }
 
   handleChange(event) {
@@ -22,44 +18,10 @@ export default class SearchBar extends Component {
 
   handleSubmit(event) {
     alert('A city was submitted: ' + this.state.term);
-    this.componentDidMount();
+    this.props.onSearchTermChange(this.state.term);
     event.preventDefault();
   }
 
-
-  componentDidMount() {
-    // event.preventDefault();
-    const city = this.state.term;
-
-    // Only make API call unless search term was saved in the state when form was sumbitted.
-    if(city != "")  {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
-        .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              isLoaded: true,
-              items: result
-            });
-            console.log('Results of API Call');
-            console.log(result);
-            console.log('Results of State');
-            console.log(this.state.items);
-
-
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
-      }
-    }
 
   render(){
       return(
--- src/index.js
+++ src/index.js
@@ -5,17 +5,51 @@ import SearchBar from './SearchBar'
 const API_KEY = '992e628de9fb7c2d45c8a82dc61259fc';
 
 class WeatherApp extends React.Component {
-
   constructor(props){
     super(props);
 
     this.state = {
-      term: '',
       error: null,
       isLoaded: false,
       items: '',
     };
+
+    this.componentDidMount = this.componentDidMount.bind(this)
   }
+
+  componentDidMount(term) {
+    // event.preventDefault();
+    const city = term;
+
+    // Only make API call unless search term was saved in the constant city when form was sumbitted.
+    if(city)  {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result
+            });
+            console.log('Results of API Call');
+            console.log(result);
+            console.log('Results of State');
+            console.log(this.state.items);
+
+
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: false,
+              error
+            });
+          }
+        )
+      }
+    }
 
 
   render(){
@@ -28,7 +62,7 @@ class WeatherApp extends React.Component {
               <li><a href="">5-day forcast</a></li>
             </ul>
 
-            <SearchBar />
+            <SearchBar onSearchTermChange={this.componentDidMount}/>
 
 
         </div>
