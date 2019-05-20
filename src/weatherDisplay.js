493bce9d604a22a12f80eec3062ba0f76d18de1c
Adam <adamh93@verizon.net>
2019-05-20 15:08:14 -0400

    done: Edit URL in the componentDidMount function for API call to use units of imperial so weather degrees come out as fahrenheit, Add temperature degrees under weather icon, Add temperature description under weather icon; task: Place 5day weather forecast in a row

--- src/weatherDisplay.js
+++ src/weatherDisplay.js
@@ -9,8 +9,7 @@ export default class WeatherAPI extends Component{
     this.state = {
       error: null,
       isLoaded: false,
-      items: '',
-
+      items: [],
     };
 
     this.componentDidMount = this.componentDidMount.bind(this)
@@ -20,23 +19,27 @@ export default class WeatherAPI extends Component{
     // event.preventDefault();
     const city = "New York";
 
+    const weather = [];
+
     // Only make API call unless search term was saved in the constant city when form was sumbitted.
     if(city)  {
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`)
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`)
         .then(res => res.json())
         .then(
           (result) => {
+            // Push weather stats for 5 days into the state variable called items
 
             for(var i = 0; i < result.list.length; i+=8){
               console.log(i);
               console.log(result.list[i]);
+              weather.push(result.list[i]);
             };
-
 
             this.setState({
               isLoaded: true,
-              items: result.list,
+              items: weather,
             });
+
             console.log(result.list.length);
 
             console.log('Results of API Call');
@@ -63,9 +66,27 @@ export default class WeatherAPI extends Component{
 
 
   render(){
-    return(
 
+    const weather = this.state.items;
+
+    const singledayForcast = weather.map((w) =>
+      <div className="weather_info">
+        <img src={`http://openweathermap.org/img/w/${w.weather[0].icon}.png`} className="icon" alt="Weather forecast icon"/>
+          <div className="weather_text">
+            {w.main.temp} &deg; F <br />
+            {w.weather[0].description}<br />
+          </div>
+      </div>
+  );
+
+
+    return(
+      <div>
       <h1>Forecast</h1>
+
+      {singledayForcast}
+
+      </div>
 
     );
   }
